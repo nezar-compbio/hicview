1d37d836cb19d8347a8dd3b4cfbbb0f2076b813b
Nezar Abdennur <nabdennur@gmail.com>
2015-02-04 00:16:13 -0500

    tidy up npy loader

--- app/hicview.js
+++ app/hicview.js
@@ -25,25 +25,143 @@
  */
 
 
+// Client-side parser for .npy files
+var NumpyLoader = (function () {
+    function bytes2ascii(buf) {
+        return String.fromCharCode.apply(null, new Uint8Array(buf));
+    }
+
+    function readUint16LE(buffer) {
+        var view = new DataView(buffer);
+        var val = view.getUint8(0);
+        val |= view.getUint8(1) << 8;
+        return val;
+    }
+
+    function fromArrayBuffer(buf) {
+      // Check the magic number
+      var magic = bytes2ascii(buf.slice(0,6));
+      if (magic.slice(1,6) != 'NUMPY') {
+          throw new Error('unknown file type');
+      }
+
+      var version = new Uint8Array(buf.slice(6,8)),
+          headerLength = readUint16LE(buf.slice(8,10)),
+          headerStr = bytes2ascii(buf.slice(10, 10+headerLength));
+          offsetBytes = 10 + headerLength;
+          //rest = buf.slice(10+headerLength);  XXX -- This makes a copy!!! https://www.khronos.org/registry/typedarray/specs/latest/#5
+
+      // Hacky conversion of dict literal string to JS Object
+      eval("var info = " + headerStr.toLowerCase().replace('(','[').replace('),',']'));
+
+      // Intepret the bytes according to the specified dtype
+      if (info.descr === "|u1") {
+          data = new Uint8Array(buf, offsetBytes);
+      } else if (info.descr === "|i1") {
+          data = new Int8Array(buf, offsetBytes);
+      } else if (info.descr === "<u2") {
+          data = new Uint16Array(buf, offsetBytes);
+      } else if (info.descr === "<i2") {
+          data = new Int16Array(buf, offsetBytes);
+      } else if (info.descr === "<u4") {
+          data = new Uint32Array(buf, offsetBytes);
+      } else if (info.descr === "<i4") {
+          data = new Int32Array(buf, offsetBytes);
+      } else if (info.descr === "<f4") {
+          data = new Float32Array(buf, offsetBytes);
+      } else if (info.descr === "<f8") {
+          data = new Float64Array(buf, offsetBytes);
+      } else {
+          throw new Error('unknown numeric dtype')
+      }
+
+      return {
+          shape: info.shape,
+          fortran_order: info.fortran_order,
+          data: data
+      };
+    }
+
+    function open(file, callback) {
+        var reader = new FileReader();
+        reader.onload = function() {
+            // the file contents have been read as an array buffer
+            var buf = reader.result;
+            var ndarray = fromArrayBuffer(buf);
+            callback(ndarray);
+        };
+        reader.readAsArrayBuffer(file);
+    }
+
+    function ajax(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function(e) {
+            var buf = xhr.response; // not responseText
+            var ndarray = fromArrayBuffer(buf);
+            callback(ndarray);
+        };
+        xhr.open("GET", url, true);
+        xhr.responseType = "arraybuffer";
+        xhr.send(null);
+    }
+
+    return {
+        open: open,
+        ajax: ajax
+    };
+})();
+ 
+
+
+
+// https://github.com/timmywil/jquery.panzoom/issues/84
+// http://bost.ocks.org/mike/chart/
+function chart() {
+  var width = 720, // default width
+      height = 80; // default height
+
+  function self() {
+    // generate chart here, using `width` and `height`
+  }
+
+  self.width = function(value) {
+    if (!arguments.length) return width;
+    width = value;
+    return self;
+  };
+
+  self.height = function(value) {
+    if (!arguments.length) return height;
+    height = value;
+    return self;
+  };
+
+  return self;
+}
+
 // Global module
 var HiCView = (function ($) {
     var self = {};
 
     var canvas,
-      Reds = d3.scale.log()
+        Reds = d3.scale.log()
             .domain([1, 2000.0])
             .range(["white", "red"]),
 
-      config = {
-        colormap: Reds
-      };
+        config = {
+           colormap: Reds
+        };
 
 
     function init() {
         canvas = document.createElement("canvas");
 
-        document.getElementById('fileloader')
-            .addEventListener('change', handleFileSelect, false);
+        $('#fileloader').on('change', function (e) {
+            var file = e.target.files[0];
+            NumpyLoader.open(file, function(ndarray) {
+                render(ndarray);
+            });
+        });
 
         return self;
     };
